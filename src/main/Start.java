399c202a34a63f99a88898a43ba4d75b424f846e
Riley Comer <2001rdc@gmail.com>
2022-03-13 03:59:50 -0800

    Gui demo is finished and hooked up to Ai files
    If you are confused about any of the code let me know. I can help walk you through it. The Ai file should be the only file that requires editing after this. All other files I added can be left alone.

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,27 +1,73 @@
 package main;
+import java.awt.event.KeyEvent;
 
-import java.util.Scanner;
-
-public class Start {
-
+public class Start implements Runnable{
+	
+	private Thread thread;
+	private Window window;
+	
+	private boolean running;
+	
+	
 	public static void main(String[] args) {
-		/* 	
-     	Purpose: the purpose of this function is a while loop that reiterates through and asks for users input
-		Input:
-		Output:
-		*/
-		Ai ai=new Ai();
-    	String out="";
-    	Scanner reader;
-    	System.out.println("I am robot");
-      	String command="";
-      	while(command!="exit"){
-        	reader = new Scanner(System.in);
-        	command = reader.nextLine().toLowerCase();
-          	out = ai.getResponse(command);
-          	System.out.println(out);
-        }
-
+		Start app=new Start();
+		app.start();
 	}
-
+	
+	private void start() {
+		if(running) {
+			return;
+		}
+		window=new Window();
+		running=true;
+		thread=new Thread((Runnable) this);
+		thread.run();
+	}
+	
+	public void run() {
+		//frames
+		double target=60.0;
+		double nsPerTick=1000000000.0/target;
+		long lastTime=System.nanoTime();
+		long timer=System.currentTimeMillis();
+		double unprocessed=0.0;
+		int fps=0;
+		int tps=0;
+		boolean canRender=false;
+		
+		while(running) {
+			long now=System.nanoTime();
+			unprocessed+=(now-lastTime)/nsPerTick;
+			lastTime=now;
+			if(unprocessed>=1.0) {
+				unprocessed--;
+				tps++;
+				canRender=true;
+				
+			}else {
+				canRender=false;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException ie){
+				System.out.println("Scanning...");
+			}
+			if(canRender) {
+				update();
+				fps++;
+			}
+			
+			if(System.currentTimeMillis()-1000>timer) {
+				timer+=1000;
+				//System.out.println("FPS: "+fps+"/nTPS: "+tps);
+				fps=0;
+				tps=0;
+			}
+		}
+		System.exit(0);
+	}
+	
+	private void update() {
+		window.update();
+	}
 }
