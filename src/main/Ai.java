a94a8bc7216cf50632ddc7d8afc5547bfe3da7b2
Riley Comer <2001rdc@gmail.com>
2022-03-19 15:00:25 -0800

    POS tagging + Tokenization
    2 toolkits included in the commit.

--- src/main/Ai.java
+++ src/main/Ai.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+ 
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
 public class Ai {
   	private String out;
   	private String[] data;
@@ -86,6 +96,67 @@ public class Ai {
                   	out+=(greetings[(int)(Math.random()*greetings.length)])+", ";
                   }
       	}
+        	
+        	
+        	InputStream tokenModelIn = null;
+            InputStream posModelIn = null;
+             
+            try {
+                String sentence = input;
+                // tokenize the sentence
+                tokenModelIn = new FileInputStream("src/main/opennlp-en-ud-ewt-tokens-1.0-1.9.3.bin");
+                TokenizerModel tokenModel = new TokenizerModel(tokenModelIn);
+                Tokenizer tokenizer = new TokenizerME(tokenModel);
+                String tokens[] = tokenizer.tokenize(sentence);
+     
+                // Parts-Of-Speech Tagging
+                // reading parts-of-speech model to a stream 
+                posModelIn = new FileInputStream("src/main/opennlp-en-ud-ewt-pos-1.0-1.9.3.bin");
+                // loading the parts-of-speech model from stream
+                POSModel posModel = new POSModel(posModelIn);
+                // initializing the parts-of-speech tagger with model 
+                POSTaggerME posTagger = new POSTaggerME(posModel);
+                // Tagger tagging the tokens
+                String tags[] = posTagger.tag(tokens);
+                // Getting the probabilities of the tags given to the tokens
+                double probs[] = posTagger.probs();
+                 
+                System.out.println("Token\t:\tTag\t:\tProbability\n---------------------------------------------");
+                for(int i=0;i<tokens.length;i++){
+                    System.out.println(tokens[i]+"\t:\t"+tags[i]+"\t:\t"+probs[i]);
+                }
+                 
+            }
+            catch (IOException e) {
+                // Model loading failed, handle the error
+                e.printStackTrace();
+            }
+            finally {
+                if (tokenModelIn != null) {
+                    try {
+                        tokenModelIn.close();
+                    }
+                    catch (IOException e) {
+                    }
+                }
+                if (posModelIn != null) {
+                    try {
+                        posModelIn.close();
+                    }
+                    catch (IOException e) {
+                    }
+                }
+            }
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
+        	
   
   		if(input.contains("where")){
   			// SkipTheDish
